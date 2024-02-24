18e2c1bf2d21af7474f6191994b0f2aa36127ac5
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-02-25 00:15:44 +0100

    Lambda challenge - more concise solution

--- src/challenge/Main.java
+++ src/challenge/Main.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
+
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         String[] firstNames = {"Bob", "Robert", "Anna", "Agnieszka", "Henry"};
 
@@ -13,40 +16,47 @@ public class Main {
         System.out.println(Arrays.toString(firstNames));
         System.out.println("-------------");
 
-        Arrays.setAll(firstNames, i -> {
-                    Random random = new Random();
+        List<String> backedByArray = Arrays.asList(firstNames);
+        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'Z') + ".");
+        System.out.println(Arrays.toString(firstNames));
+
+        /*Arrays.setAll(firstNames, i -> {
                     int initialNumber = random.nextInt(65, 91);
-                    char randomChar = (char) initialNumber;
-                    return firstNames[i] + " " + randomChar + ".";
+                    return firstNames[i] + " " + (char) initialNumber + ".";
                 }
         );
         System.out.println(Arrays.toString(firstNames));
-        System.out.println("-------------");
+        System.out.println("-------------");*/
 
-        Arrays.setAll(firstNames, i -> {
+        /*Arrays.setAll(firstNames, i -> {
             String copyOfName = firstNames[i];
             String[] partsOfName = splitString(copyOfName, 2);
             StringBuilder nameToReverse = new StringBuilder(partsOfName[0]);
-            nameToReverse.reverse();
-            String surname = nameToReverse.toString();
-            return firstNames[i] + " " + surname;
-        });
+            return firstNames[i] + " " + nameToReverse.reverse().toString();
+        });*/
 
+        backedByArray.replaceAll(s -> s += " " + getReversedName(s.split(" ")[0]));
 
-        List<String> modifiableList = new ArrayList<>(Arrays.asList(firstNames));
-        modifiableList.forEach(s -> System.out.println(s));
+        Arrays.asList(firstNames).forEach(s -> System.out.println(s));
         System.out.println("-------------");
 
+        List<String> newList = new ArrayList<>(List.of(firstNames));
 
-        modifiableList.removeIf(n -> {
-            String[] partsOfName = splitString(n,3);
-            return partsOfName[0].equals(partsOfName[2]);
-        });
-        modifiableList.forEach(s -> System.out.println(s));
+        newList.removeIf(n -> n.substring(0, n.indexOf(" ")).equals(n.substring(n.lastIndexOf(" ") + 1))
+        );
+        newList.forEach(s -> System.out.println(s));
         System.out.println("-------------");
     }
 
-    public static String[] splitString(String toSplit, int limit){
+    public static String[] splitString(String toSplit, int limit) {
         return toSplit.split(" ", limit);
+    }
+
+    public static char getRandomChar(char startChar, char endChar) {
+        return (char) random.nextInt((int) startChar, (int) endChar + 1);
+    }
+
+    private static String getReversedName(String firstName) {
+        return new StringBuilder(firstName).reverse().toString();
     }
 }
