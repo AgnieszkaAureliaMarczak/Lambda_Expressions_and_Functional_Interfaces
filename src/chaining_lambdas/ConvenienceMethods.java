e43073544a608456ba55d44271ed44171def8072
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-03-01 00:17:03 +0100

    Predicate and Comparator  convenience methods

--- src/chaining_lambdas/ConvenienceMethods.java
+++ src/chaining_lambdas/ConvenienceMethods.java
@@ -1,8 +1,12 @@
 package chaining_lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class ConvenienceMethods {
     public static void main(String[] args) {
@@ -10,7 +14,7 @@ public class ConvenienceMethods {
         Function<String, String> uCase = String::toUpperCase;
         System.out.println(uCase.apply(name));
 
-        Function<String, String> lastName = s->s.concat(" Buchalka");
+        Function<String, String> lastName = s -> s.concat(" Buchalka");
         Function<String, String> uCaseLastName = uCase.andThen(lastName);
         System.out.println(uCaseLastName.apply(name));
 
@@ -32,7 +36,7 @@ public class ConvenienceMethods {
                 .andThen(s -> s.concat(" Buchalka"))
                 .andThen(s -> s.split(" "))
                 .andThen(s -> String.join(", ", s))
-                .andThen(String:: length);
+                .andThen(String::length);
         System.out.println(f2.apply(name));
 
         String[] names = {"Ann", "Bob", "Carol"};
@@ -42,6 +46,44 @@ public class ConvenienceMethods {
                 .andThen(s -> System.out.print(" - "))
                 .andThen(s1));
 
+        Predicate<String> p1 = s -> s.equals("TIM");
+        Predicate<String> p2 = s -> s.equalsIgnoreCase("Tim");
+        Predicate<String> p3 = s -> s.startsWith("T");
+        Predicate<String> p4 = s -> s.endsWith("e");
 
+        Predicate<String> combined1 = p1.or(p2);
+        System.out.println("combined1 = " + combined1.test(name));
+
+        Predicate<String> combined2 = p3.and(p4);
+        System.out.println("combined2 = " + combined2.test(name));
+
+        Predicate<String> combined3 = p3.and(p4).negate();
+        System.out.println("combined3 = " + combined3.test(name));
+
+        record Person(String firstName, String lastName){}
+
+        List<Person> list = new ArrayList<>(Arrays.asList(
+                new Person("Peter", "Pan"),
+                new Person("Peter", "PumpkinEater"),
+                new Person("Minnie", "Mouse"),
+                new Person("Mickey", "Mouse")
+        ));
+
+        list.sort((o1, o2) -> o1.lastName.compareTo(o2.lastName));
+        list.forEach(System.out::println);
+
+        System.out.println("--------------------");
+        list.sort(Comparator.comparing(Person::lastName));
+        list.forEach(System.out::println);
+
+        System.out.println("--------------------");
+        list.sort(Comparator.comparing(Person::lastName)
+                        .thenComparing(Person::firstName));
+        list.forEach(System.out::println);
+
+        System.out.println("--------------------");
+        list.sort(Comparator.comparing(Person::lastName)
+                .thenComparing(Person::firstName).reversed());
+        list.forEach(System.out::println);
     }
 }
