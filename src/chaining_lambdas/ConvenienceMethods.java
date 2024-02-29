07257a93e5cf3b42e9cc27c7978e4d6d1c3a06e9
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-02-29 23:31:27 +0100

    Function and Consumer interface convenience methods

---
+++ src/chaining_lambdas/ConvenienceMethods.java
@@ -0,0 +1,47 @@
+package chaining_lambdas;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class ConvenienceMethods {
+    public static void main(String[] args) {
+        String name = "Tim";
+        Function<String, String> uCase = String::toUpperCase;
+        System.out.println(uCase.apply(name));
+
+        Function<String, String> lastName = s->s.concat(" Buchalka");
+        Function<String, String> uCaseLastName = uCase.andThen(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        uCaseLastName = uCase.compose(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s.split(" "));
+        System.out.println(Arrays.toString(f0.apply(name)));
+
+        Function<String, String> f1 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s.split(" "))
+                .andThen(s -> s[1].toUpperCase() + ", " + s[0]);
+        System.out.println(f1.apply(name));
+
+        Function<String, Integer> f2 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s.split(" "))
+                .andThen(s -> String.join(", ", s))
+                .andThen(String:: length);
+        System.out.println(f2.apply(name));
+
+        String[] names = {"Ann", "Bob", "Carol"};
+        Consumer<String> s0 = s -> System.out.println(s.charAt(0));
+        Consumer<String> s1 = System.out::println;
+        Arrays.asList(names).forEach(s0
+                .andThen(s -> System.out.print(" - "))
+                .andThen(s1));
+
+
+    }
+}
